18efa66707a258f75bc4a31d540dbe8c1ad05e0f
steven69 <steven62885@gmail.com>
2022-10-22 15:34:11 +0800

    Initial Commit

---
+++ ioshw2_00857039/ioshw2_00857039App.swift
@@ -0,0 +1,17 @@
+//
+//  ioshw2_00857039App.swift
+//  ioshw2_00857039
+//
+//  Created by User20 on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ioshw2_00857039App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
